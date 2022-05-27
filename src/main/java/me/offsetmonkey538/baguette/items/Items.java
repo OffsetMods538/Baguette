4a27347346f408884ba5cfe7194f51a6d6948fa1
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-05-27 19:07:03 +0300

    Add Long Baguette Sword

--- src/main/java/me/offsetmonkey538/baguette/items/Items.java
+++ src/main/java/me/offsetmonkey538/baguette/items/Items.java
@@ -1,7 +1,10 @@
 package me.offsetmonkey538.baguette.items;
 
+import me.offsetmonkey538.baguette.items.tools.LongBaguetteSword;
+import me.offsetmonkey538.baguette.items.tools.LongBaguetteToolMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ToolItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -10,11 +13,12 @@ public class Items {
     public static final Item LONG_BAGUETTE = new LongBaguette(new Item.Settings().group(ItemGroup.FOOD));
     public static final Item TNT_BAGUETTE = new TntBaguette(new Item.Settings().group(ItemGroup.FOOD));
     public static final Item WATER_BAGUETTE = new WaterBaguette(new Item.Settings().group(ItemGroup.FOOD));
-
+    public static final ToolItem LONG_BAGUETTE_SWORD = new LongBaguetteSword(new LongBaguetteToolMaterial(), 4, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("baguette", "baguette"), BAGUETTE);
         Registry.register(Registry.ITEM, new Identifier("baguette", "long_baguette"), LONG_BAGUETTE);
         Registry.register(Registry.ITEM, new Identifier("baguette", "tnt_baguette"), TNT_BAGUETTE);
         Registry.register(Registry.ITEM, new Identifier("baguette", "water_baguette"), WATER_BAGUETTE);
+        Registry.register(Registry.ITEM, new Identifier("baguette", "long_baguette_sword"), LONG_BAGUETTE_SWORD);
     }
 }
---
+++ src/main/java/me/offsetmonkey538/baguette/items/tools/LongBaguetteSword.java
@@ -0,0 +1,11 @@
+package me.offsetmonkey538.baguette.items.tools;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class LongBaguetteSword extends SwordItem {
+
+    public LongBaguetteSword(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+}
