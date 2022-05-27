fadb3fa73e83d3b48ce58487a2598cae287c0584
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-05-27 19:12:02 +0300

    Make it easier to add items

--- src/main/java/me/offsetmonkey538/baguette/MainBaguette.java
+++ src/main/java/me/offsetmonkey538/baguette/MainBaguette.java
@@ -1,7 +1,6 @@
 package me.offsetmonkey538.baguette;
 
 import me.offsetmonkey538.baguette.config.JsonConfig;
-import me.offsetmonkey538.baguette.items.Items;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +12,6 @@ public class MainBaguette implements ModInitializer {
     public void onInitialize() {
         JsonConfig.loadConfig();
         LOGGER.info("Configuration file loaded!");
-        Items.register();
-        LOGGER.info("Items registered!");
 
         LOGGER.info("Baguette has been initialized!");
     }
--- src/main/java/me/offsetmonkey538/baguette/items/Items.java
+++ src/main/java/me/offsetmonkey538/baguette/items/Items.java
@@ -9,16 +9,14 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Items {
-    public static final Item BAGUETTE = new Baguette(new Item.Settings().group(ItemGroup.FOOD));
-    public static final Item LONG_BAGUETTE = new LongBaguette(new Item.Settings().group(ItemGroup.FOOD));
-    public static final Item TNT_BAGUETTE = new TntBaguette(new Item.Settings().group(ItemGroup.FOOD));
-    public static final Item WATER_BAGUETTE = new WaterBaguette(new Item.Settings().group(ItemGroup.FOOD));
-    public static final ToolItem LONG_BAGUETTE_SWORD = new LongBaguetteSword(new LongBaguetteToolMaterial(), 4, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
-    public static void register() {
-        Registry.register(Registry.ITEM, new Identifier("baguette", "baguette"), BAGUETTE);
-        Registry.register(Registry.ITEM, new Identifier("baguette", "long_baguette"), LONG_BAGUETTE);
-        Registry.register(Registry.ITEM, new Identifier("baguette", "tnt_baguette"), TNT_BAGUETTE);
-        Registry.register(Registry.ITEM, new Identifier("baguette", "water_baguette"), WATER_BAGUETTE);
-        Registry.register(Registry.ITEM, new Identifier("baguette", "long_baguette_sword"), LONG_BAGUETTE_SWORD);
+    public static final Item BAGUETTE = register(new Baguette(new Item.Settings().group(ItemGroup.FOOD)), "baguette");
+    public static final Item LONG_BAGUETTE = register(new LongBaguette(new Item.Settings().group(ItemGroup.FOOD)), "long_baguette");
+    public static final Item TNT_BAGUETTE = register(new TntBaguette(new Item.Settings().group(ItemGroup.FOOD)), "tnt_baguette");
+    public static final Item WATER_BAGUETTE = register(new WaterBaguette(new Item.Settings().group(ItemGroup.FOOD)), "water_baguette");
+    public static final ToolItem LONG_BAGUETTE_SWORD = (ToolItem) register(new LongBaguetteSword(new LongBaguetteToolMaterial(), 4, -2.4F, new Item.Settings().group(ItemGroup.COMBAT)), "long_baguette_sword");
+
+    private static Item register(Item item, String name) {
+        Registry.register(Registry.ITEM, new Identifier("baguette", name), item);
+        return item;
     }
 }
