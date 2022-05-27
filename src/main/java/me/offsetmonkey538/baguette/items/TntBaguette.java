6f29cd6db3f16594d384872861cc3ba63ad7fbc4
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-05-27 13:55:33 +0300

    Make TntBaguette use explosion strength from config

--- src/main/java/me/offsetmonkey538/baguette/items/TntBaguette.java
+++ src/main/java/me/offsetmonkey538/baguette/items/TntBaguette.java
@@ -1,5 +1,6 @@
 package me.offsetmonkey538.baguette.items;
 
+import me.offsetmonkey538.baguette.config.JsonConfig;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
@@ -17,8 +18,7 @@ public class TntBaguette extends Item {
 
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        // TODO: Add configuration for explosion power
-        world.createExplosion(user, user.getX(), user.getY(), user.getZ(), 6.0f, Explosion.DestructionType.BREAK);
+        world.createExplosion(user, user.getX(), user.getY(), user.getZ(), JsonConfig.getConfig().tntBaguetteExplosionStrength(), Explosion.DestructionType.BREAK);
         return super.finishUsing(stack, world, user);
     }
 }
