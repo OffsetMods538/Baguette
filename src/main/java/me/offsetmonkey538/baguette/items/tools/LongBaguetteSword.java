3ca8fdb25dbbf64a372f5954880c66748993feac
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-05-28 14:15:47 +0300

    Give Baguette Sword special ability: When a player that doesn't have a full hunger bar gets hit with the sword, the player gets the hunger points of 1 bread added to their hunger, but the sword gets damaged by one 11th (the sword is made with 11 bread) of its durability.

--- src/main/java/me/offsetmonkey538/baguette/items/tools/LongBaguetteSword.java
+++ src/main/java/me/offsetmonkey538/baguette/items/tools/LongBaguetteSword.java
@@ -1,11 +1,24 @@
 package me.offsetmonkey538.baguette.items.tools;
 
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolMaterial;
 
 public class LongBaguetteSword extends SwordItem {
 
     public LongBaguetteSword(Settings settings) {
         super(new LongBaguetteToolMaterial(), 4, -3.1F, settings);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        super.postHit(stack, target, attacker);
+        if (target instanceof PlayerEntity player && player.getHungerManager().isNotFull()) {
+            player.getHungerManager().add(5, 0.6f);
+            stack.damage(25, attacker, (e) -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+        }
+        return true;
     }
 }
