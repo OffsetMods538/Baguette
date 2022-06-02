8bba84d9d5b5fb28746467cc266403466a7ff725
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-06-02 23:01:05 +0300

    Refactor

--- src/main/java/me/offsetmonkey538/baguette/items/baguettes/LongBaguette.java
+++ src/main/java/me/offsetmonkey538/baguette/items/baguettes/LongBaguette.java
@@ -1,15 +1,12 @@
 package me.offsetmonkey538.baguette.items.baguettes;
 
-import me.offsetmonkey538.baguette.items.ModItems;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 
-import java.util.Objects;
-
 public class LongBaguette extends Baguette {
     protected static final FoodComponent FOOD_COMPONENT = new FoodComponent.Builder()
-            .hunger(Objects.requireNonNull(ModItems.Items.BAGUETTE.getItem().getFoodComponent()).getHunger() * 3)
-            .saturationModifier(ModItems.Items.BAGUETTE.getItem().getFoodComponent().getSaturationModifier() * 3)
+            .hunger(Baguette.FOOD_COMPONENT.getHunger() * 3)
+            .saturationModifier(Baguette.FOOD_COMPONENT.getSaturationModifier() * 3)
             .build();
 
     public LongBaguette(Item.Settings settings) {
