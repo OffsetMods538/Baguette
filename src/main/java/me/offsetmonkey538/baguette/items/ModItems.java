d9600477e6ef496eb9dab561672243fd6d6a44f3
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-06-04 14:40:42 +0300

    Make ModItems.java not so complicated. And also make it easier to get items.

--- src/main/java/me/offsetmonkey538/baguette/items/ModItems.java
+++ src/main/java/me/offsetmonkey538/baguette/items/ModItems.java
@@ -15,24 +15,24 @@ public class ModItems {
     private static final Map<String, Item> items = new HashMap<>();
 
 
-    public static final Item BAGUETTE             = addItem(new Baguette(new Item.Settings().group(ItemGroup.FOOD)), "baguette");
-    public static final Item LONG_BAGUETTE        = addItem(new LongBaguette(new Item.Settings().group(ItemGroup.FOOD)), "long_baguette");
-    public static final Item TNT_BAGUETTE         = addItem(new TntBaguette(new Item.Settings().group(ItemGroup.FOOD)), "tnt_baguette");
-    public static final Item CHARGED_TNT_BAGUETTE = addItem(new ChargedTntBaguette(new Item.Settings().group(ItemGroup.FOOD)), "charged_tnt_baguette");
-    public static final Item WATER_BAGUETTE       = addItem(new WaterBaguette(new Item.Settings().group(ItemGroup.FOOD)), "water_baguette");
-    public static final Item LONG_BAGUETTE_SWORD  = addItem(new LongBaguetteSword(new Item.Settings().group(ItemGroup.COMBAT)), "long_baguette_sword");
-    public static final Item EASTEREGG_BAGUETTE   = addItem(new EastereggBaguette(new Item.Settings().group(ItemGroup.FOOD)), "easteregg_baguette");
-    public static final Item BIRTHDAY_BAGUETTE    = addItem(new BirthdayBaguette(new Item.Settings().group(ItemGroup.FOOD)), "birthday_baguette");
+    public static final Item BAGUETTE             = addItem(new Baguette(           new Item.Settings().group(ItemGroup.FOOD)),   "baguette");
+    public static final Item LONG_BAGUETTE        = addItem(new LongBaguette(       new Item.Settings().group(ItemGroup.FOOD)),   "long_baguette");
+    public static final Item TNT_BAGUETTE         = addItem(new TntBaguette(        new Item.Settings().group(ItemGroup.FOOD)),   "tnt_baguette");
+    public static final Item CHARGED_TNT_BAGUETTE = addItem(new ChargedTntBaguette( new Item.Settings().group(ItemGroup.FOOD)),   "charged_tnt_baguette");
+    public static final Item WATER_BAGUETTE       = addItem(new WaterBaguette(      new Item.Settings().group(ItemGroup.FOOD)),   "water_baguette");
+    public static final Item LONG_BAGUETTE_SWORD  = addItem(new LongBaguetteSword(  new Item.Settings().group(ItemGroup.COMBAT)), "long_baguette_sword");
+    public static final Item EASTEREGG_BAGUETTE   = addItem(new EastereggBaguette(  new Item.Settings().group(ItemGroup.FOOD)),   "easteregg_baguette");
+    public static final Item BIRTHDAY_BAGUETTE    = addItem(new BirthdayBaguette(   new Item.Settings().group(ItemGroup.FOOD)),   "birthday_baguette");
 
+
+    private static Item addItem(Item item, String name) {
+        items.put(name, item);
+        return item;
+    }
 
     public static void register() {
         for (Map.Entry<String, Item> entry : items.entrySet()) {
             Registry.register(Registry.ITEM, new Identifier(BaguetteMain.MOD_ID, entry.getKey()), entry.getValue());
         }
-    }
-
-    private static Item addItem(Item item, String name) {
-        items.put(name, item);
-        return item;
     }
 }
