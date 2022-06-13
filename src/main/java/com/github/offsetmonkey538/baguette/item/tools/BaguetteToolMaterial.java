7de8602dda5eb1d804f151cec649d333d5efe73d
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-06-13 21:51:08 +0300

    Refactoring

--- src/main/java/com/github/offsetmonkey538/baguette/item/tools/BaguetteToolMaterial.java
+++ src/main/java/com/github/offsetmonkey538/baguette/item/tools/BaguetteToolMaterial.java
@@ -4,7 +4,7 @@ import net.minecraft.item.Items;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.recipe.Ingredient;
 
-public class LongBaguetteToolMaterial implements ToolMaterial {
+public class BaguetteToolMaterial implements ToolMaterial {
     @Override
     public int getDurability() {
         return 275;
