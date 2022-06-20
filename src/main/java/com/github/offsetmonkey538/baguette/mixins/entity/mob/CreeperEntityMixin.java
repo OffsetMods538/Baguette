4d64efe8f389b6879790e050a571be2fcb750623
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-06-20 11:46:54 +0300

    Make `CreeperEntityMixin.dropEquipment()` only run server-side

--- src/main/java/com/github/offsetmonkey538/baguette/mixins/entity/mob/CreeperEntityMixin.java
+++ src/main/java/com/github/offsetmonkey538/baguette/mixins/entity/mob/CreeperEntityMixin.java
@@ -5,8 +5,8 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,7 +22,7 @@ public class CreeperEntityMixin extends HostileEntity {
 
     @Inject(method = "dropEquipment", at = @At("TAIL"))
     public void dropEquipment(DamageSource source, int lootingMultiplier, boolean allowDrops, CallbackInfo ci) {
-        if (source.getAttacker() instanceof PlayerEntity player) {
+        if (source.getAttacker() instanceof ServerPlayerEntity player) {
             if (player.getActiveItem().getItem().equals(ModItems.CHARGED_TNT_BAGUETTE)) {
                 this.dropItem(Items.CREEPER_HEAD);
             }
