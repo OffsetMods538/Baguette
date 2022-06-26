db8558c68df5068c8de9463db28b038faa9b1e36
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-06-26 15:39:36 +0300

    More stuff with comments. Add constants for `maxHealth`, `minHealth`, `healthIncrement` and `healthDecrement`

--- src/main/java/com/github/offsetmonkey538/baguette/item/baguettes/QuestionBaguette.java
+++ src/main/java/com/github/offsetmonkey538/baguette/item/baguettes/QuestionBaguette.java
@@ -37,31 +37,41 @@ public class QuestionBaguette extends Baguette {
     @Environment(EnvType.CLIENT)
     private final Random rng = Random.create();
 
-    // Map of a Player to their Respective question
+    // Map of a Player to their respective question
     @Environment(EnvType.CLIENT)
     private final Map<ClientPlayerEntity, Integer> players = new HashMap<>();
 
-    // Array of questions
     @Environment(EnvType.CLIENT)
     private String[] questions;
 
-    // Array of answers
     @Environment(EnvType.CLIENT)
     private String[] answers;
+
+    @Environment(EnvType.CLIENT)
+    private static final float maxHealth = 40;
+
+    @Environment(EnvType.CLIENT)
+    private static final float minHealth = 2;
+
+    @Environment(EnvType.CLIENT)
+    private static final float healthIncrement = 2;
+
+    @Environment(EnvType.CLIENT)
+    private static final float healthDecrement = 2;
 
     public QuestionBaguette(Settings settings) {
         super(settings);
 
-        // Register packet receiver for adding health to a player
+        // Register packet for adding health to a player
         ServerPlayNetworking.registerGlobalReceiver(ADD_HEALTH_ID, (server, player, handler, buf, responseSender) -> {
-            int health = buf.readInt();
+            float health = buf.readFloat();
 
             server.execute(() -> addHealth(player, health));
         });
 
-        // Register packet receiver for removing health from a player
+        // Register packet for removing health from a player
         ServerPlayNetworking.registerGlobalReceiver(REMOVE_HEALTH_ID, (server, player, handler, buf, responseSender) -> {
-            int health = buf.readInt();
+            float health = buf.readFloat();
 
             server.execute(() -> removeHealth(player, health));
         });
@@ -84,8 +94,12 @@ public class QuestionBaguette extends Baguette {
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         if (user instanceof ClientPlayerEntity player) {
-            int questionNumber = rng.nextBetween(0, (questions.length - 1));    // Chooses a random question
-            players.put(player, questionNumber);    // Adds the player and question to the player-question map.
+            // Chooses a random question
+            int questionNumber = rng.nextBetween(0, (questions.length - 1));
+
+            // Adds the player and question to the player-question map.
+            players.put(player, questionNumber);
+
 
             // Send the question to the player.
             player.sendMessage(Text.of(questions[players.get(player)]));
@@ -95,10 +109,10 @@ public class QuestionBaguette extends Baguette {
     }
 
     /**
-     * Checks if the sent message is the correct or incorrect answer. Adds and removes max health respectively.
+     * Checks if the sent message is the correct or incorrect answer.
      * @param message The message the player sent.
      * @param sender The player that sent the message.
-     * @return True if the player doesn't have a pending question. False if they do.
+     * @return False if the player has a question. True if they don't.
      */
     @Environment(EnvType.CLIENT)
     public boolean onMessage(String message, ClientPlayerEntity sender) {
@@ -107,10 +121,11 @@ public class QuestionBaguette extends Baguette {
             // Checks if the sender is in the entry.
             if (entry.getKey().getUuid().equals(sender.getUuid())) {
                 // Checks if the answer is correct. Not case-sensitive.
-                if (message.equalsIgnoreCase(answers[entry.getValue()])) answeredCorrectly(sender, message);
+                if (message.equalsIgnoreCase(answers[entry.getValue()])) answeredCorrectly(message, sender);
                 else answeredIncorrectly(sender, message, entry);
 
-                players.remove(entry.getKey());    // Removes the player from the player-question map.
+                // Removes the player from the player-question map.
+                players.remove(entry.getKey());
                 return false;
             }
         }
@@ -118,49 +133,86 @@ public class QuestionBaguette extends Baguette {
         return true;
     }
 
+    /**
+     * Tells the player their answer is correct. Sends a packet to the server to add max health.
+     * @param message The message the player sent.
+     * @param player The player that sent the message.
+     */
     @Environment(EnvType.CLIENT)
-    private void answeredCorrectly(ClientPlayerEntity player, String message) {
+    private void answeredCorrectly(String message, ClientPlayerEntity player) {
         // Tells the player their answer is correct.
         player.sendMessage(Text.translatable((this.getTranslationKey() + ".correct"), message));
 
-        if (player.getHealth() + 2 > 40)    // Adds a maximum health limit of 40.
+        if (player.getMaxHealth() + healthIncrement > maxHealth) {
+            // Tells the player that they are at the maximum health.
             player.sendMessage(Text.translatable(this.getTranslationKey() + ".maximum_health"));
-        else {
-            PacketByteBuf buf = PacketByteBufs.create();
-            buf.writeInt(2);
-            ClientPlayNetworking.send(ADD_HEALTH_ID, buf);    // Sends a packet telling the server to change the max health of the player.
+            return;
         }
+
+        // Creates a buffer and stores the amount of health to add to the player.
+        PacketByteBuf buf = PacketByteBufs.create();
+        buf.writeFloat(healthIncrement);
+
+        // Sends a packet telling the server to change the max health of the player.
+        ClientPlayNetworking.send(ADD_HEALTH_ID, buf);
     }
 
+    /**
+     * Tells the player their answer is incorrect. Sends a packet to the server to remove max health.
+     * @param message The message the player sent.
+     * @param player The player that sent the message.
+     */
     @Environment(EnvType.CLIENT)
     private void answeredIncorrectly(ClientPlayerEntity player, String message, Map.Entry<ClientPlayerEntity, Integer> entry) {
         // Tells the player their answer is incorrect. Also tells them the correct answer.
         player.sendMessage(Text.translatable((this.getTranslationKey() + ".incorrect"), message, answers[entry.getValue()]));
 
-        if (player.getMaxHealth() - 2 <= 0)    // Adds a minimum health limit of 2.
+        if (player.getMaxHealth() - healthDecrement < minHealth) {
+            // Tells the player that they are at the minimum health.
             player.sendMessage(Text.translatable(this.getTranslationKey() + ".minimum_health"));
-        else {
-            PacketByteBuf buf = PacketByteBufs.create();
-            buf.writeInt(2);
-            ClientPlayNetworking.send(REMOVE_HEALTH_ID, buf);    // Sends a packet telling the server to change the max health of the player.
+            return;
         }
+
+        // Creates a buffer and stores the amount of health to add to the player.
+        PacketByteBuf buf = PacketByteBufs.create();
+        buf.writeFloat(healthDecrement);
+
+        // Sends a packet telling the server to change the max health of the player.
+        ClientPlayNetworking.send(REMOVE_HEALTH_ID, buf);
+
     }
 
-
+    /**
+     * Adds max health to the player.
+     * @param player The player whose max health will be increased.
+     * @param health The amount of max health to give the player.
+     */
     private void addHealth(ServerPlayerEntity player, double health) {
+        // Get the MAX HEALTH attribute for the player.
         EntityAttributeInstance attributeInstance = player.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
 
+        // Check if the attribute instance is null.
         if (attributeInstance != null)
+            // Add a modifier for the max health.
             attributeInstance.addPersistentModifier(new EntityAttributeModifier(this.getTranslationKey(), health, EntityAttributeModifier.Operation.ADDITION));
     }
 
-
+    /**
+     * Removes max health from the player.
+     * @param player The player whose max health will be decreased.
+     * @param health The amount of max health to remove from the player.
+     */
     private void removeHealth(ServerPlayerEntity player, double health) {
+        // Get the MAX HEALTH attribute for the player.
         EntityAttributeInstance attributeInstance = player.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
 
+        // Check if the attribute instance is null.
         if (attributeInstance != null)
+            // Add a modifier for the max health.
             attributeInstance.addPersistentModifier(new EntityAttributeModifier(this.getTranslationKey(), -health, EntityAttributeModifier.Operation.ADDITION));
+        // Check if the health of the player is more than the new max health.
         if (player.getHealth() > player.getMaxHealth())
+            // Set the health of the player to the max health.
             player.setHealth(player.getMaxHealth());
     }
 }
